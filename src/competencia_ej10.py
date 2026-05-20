a7bb43c6fdc7b74847a1b582fb55214f08d8c075
Matias Vega <vegamatiascorreo@gmail.com>
2026-05-20 11:36:18 -0300

    Resolucion del ejercicio 10

--- src/competencia_ej10.py
+++ src/competencia_ej10.py
@@ -1,26 +1,94 @@
-#5 participantes evaluados por 3 jueces durante 5 rondas
-#Cada juez otorga del 1 al 10
-#Puntaje de ronda = suma de los 3 jueces
-#Imprimir: 
-# tabla de posiciones parcial al finalizar una ronda
-# ganador de la última ronda
-# contabilizar rondas ganadas
-# tabla final con puntaje total, cantida de rondas ganadas,
-# mejor puntaje de ronda, y puntaje promedio por ronda.
-# (todo en orden decreciente por puntaje total)
-
-def simular_ronda(actual, nombres):
-    #Inicializo lista del puntaje de ronda
-    suma = [0,0,0,0,0]
+def simular_ronda(actual, parcial):
+    """Calcula el puntaje de cada competidor en la ronda actual
+    (suma de los tres jueces) y lo guarda en una lista. Además
+    calcula quién fue el ganador de la ronda.
     
+    Parámetros
+    - actual: diccionario de la clave 'scores'. Contiene el par 
+    'nombre': {puntaje de los jueces}
+    - parcial: diccionario que almacena el resultado de la ronda actual
+    
+    Retorno
+    - ganador: nombre del ganador de la ronda
+    """
+
     #Itero sobre el diccionario 'scores'
     for indice,competidor in enumerate(actual):
-        #Obtengo el puntaje de ronda de cada competidor y lo guardo en la lista
+        #Guardo el puntaje de cada participante
         puntajes = list(actual[competidor].values())
-        suma[indice] = sum(puntajes)
+        parcial[competidor] = sum(puntajes)
     
-    #Busco el índice del puntaje máximo de la ronda y 
-    #me guardo el nombre del competidor correspondiente
-    ganador = nombres[suma.index(max(suma))]
+    #Obtengo el nombre del ganador
+    ganador = max(parcial, key=parcial.get)
     
-    return ganador, suma
+    return ganador
+
+def imprimir_posiciones(parcial, numero, titulo):
+    """Muestra los resultados de la última ronda.
+    Ordena el diccionario con los resultados parciales y
+    muestra el puntaje de cada competidor de forma descendente
+    
+    Parámetros:
+    - parcial: diccionario que contiene el par 'nombre': puntaje de ronda
+    - numero: de la última ronda
+    - titulo: de la última ronda
+    """
+    #Ordeno el diccionario
+    parcial = dict(sorted(parcial.items(), key= lambda elem: elem[1], reverse= True))
+    
+    #Variables usadas dentro del for
+    mensaje = f'Ronda {numero} - {titulo}:\n'
+    primero = True
+    
+    for competidor in parcial:
+        #Voy añadiendo la información a la cadena mensaje
+        if primero:
+            mensaje +=f'Ganador/a: {competidor} ({parcial[competidor]})\n'
+            primero = False
+        else:
+            mensaje += f'{competidor} ({parcial[competidor]})\n'
+    
+    #Imprimo la tabla de posiciones
+    print(mensaje)
+
+def guardar_ronda(competidores, parcial, ganador):
+    """Guarda la información procesada por simular_ronda() en un diccionario.
+    
+    Parámetros
+    - competidores: el diccionario. Contiene el par 
+    'nombre': [puntaje total, puntaje maximo, rondas ganadas]
+    - parcial: diccionario que almacena el resultado de la última ronda
+    - ganador: de la última ronda
+        
+    """
+    #Itero sobre el diccionario
+    for indice,competidor in enumerate(competidores):
+        #Sumo el puntaje de la ronda anterior al puntaje total
+        competidores[competidor][0] += parcial[competidor]
+        #Actualizo el maximo de puntaje de ronda
+        if parcial[competidor] > competidores[competidor][1]:
+            competidores[competidor][1] = parcial[competidor]
+    
+    #Sumo una victoria al ganador  
+    competidores[ganador][2] += 1
+
+def imprimir_final(competidores):
+    """Muestra una tabla con toda la información de la competencia.
+
+    Parámetros:
+    - competidores: diccionario que contiene el par 
+    'nombre': [puntaje total, puntaje maximo, rondas ganadas]
+        
+    """
+    #Ordeno el diccionario de forma descendente
+    competidores = dict(sorted(competidores.items(), key= lambda elem: elem[1][0], reverse= True))
+    
+    #Imprimo la tabla
+    titulo = 'Tabla de posiciones final:\nCocinero     Puntaje     Rondas ganadas     Mejor Ronda     Promedio'
+    separador = '-'*70
+    print(titulo)
+    print(separador)
+    
+    for competidor in competidores:
+        resultado = f'{competidor:<13} {competidores[competidor][0]:<15} {competidores[competidor][2]:<17} {competidores[competidor][1]:<13} {competidores[competidor][0] / 5:.1f}'
+        print(resultado)
